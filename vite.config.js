39575b02c72bc176c6d6b61cc691f59f328b3d27
madyardwn <madyardwn@gmail.com>
2023-09-23 19:15:29 +0700

    integrate tablar

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,20 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/js/app.js', 'resources/css/app.css'],
             refresh: true,
         }),
     ],
+    server: {
+        hmr: {
+            host: 'localhost',
+            protocol: 'ws',
+            port: 3000
+        }
+    },
+    build: {
+        commonjsOptions: {
+            transformMixedEsModules: true
+        }
+    }
 });
