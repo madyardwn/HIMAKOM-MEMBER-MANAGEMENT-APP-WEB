d96cd540f3c2b3898f37626a5634d2f7387da80c
madyardwn <madyardwn@gmail.com>
2023-10-26 23:00:48 +0700

    fix validation message

--- resources/views/pages/users/_add.blade.php
+++ resources/views/pages/users/_add.blade.php
@@ -52,7 +52,7 @@
                                     <div class="mb-3">
                                         <label class="form-label">Password</label>
                                         <div class="input-group input-group-flat">
-                                            <input type="password" name="password" class="type-password form-control" placeholder="Password" autocomplete="off">
+                                            <input type="password" name="password" id="add-password" class="type-password form-control" placeholder="Password" autocomplete="off">
                                             <span class="input-group-text">
                                                 <a href="#" class="link-secondary" data-bs-toggle="tooltip"><!-- Download SVG icon from http://tabler-icons.io/i/eye -->
                                                     <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
@@ -69,7 +69,7 @@
                                     <div class="mb-3">
                                         <label class="form-label">Confirm Password</label>
                                         <div class="input-group input-group-flat">
-                                            <input type="password" name="password_confirmation" class="type-password form-control" placeholder="Password" autocomplete="off">
+                                            <input type="password" name="password_confirmation" id="add-password_confirmation" class="type-password form-control" placeholder="Password" autocomplete="off">
                                             <span class="input-group-text">
                                                 <a href="#" class="link-secondary" data-bs-toggle="tooltip"><!-- Download SVG icon from http://tabler-icons.io/i/eye -->
                                                     <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
