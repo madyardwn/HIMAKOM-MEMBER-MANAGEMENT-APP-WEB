3cccf03e186b78b8a17aed292606a15e93226dd9
madyardwn <madyardwn@gmail.com>
2023-11-11 09:43:47 +0700

    add class type-password

--- resources/views/vendor/tablar/auth/login.blade.php
+++ resources/views/vendor/tablar/auth/login.blade.php
@@ -27,7 +27,7 @@
                             </span>
                         </label>
                         <div class="input-group input-group-flat">
-                            <input type="password" name="password" class="form-control @error('password') is-invalid @enderror" placeholder="Your password" autocomplete="off">
+                            <input type="password" name="password" class="form-control @error('password') is-invalid @enderror type-password" placeholder="Your password" autocomplete="off">
                             <span class="input-group-text">
                                 <a href="#" class="link-secondary" title="Show password" data-bs-toggle="tooltip"><!-- Download SVG icon from http://tabler-icons.io/i/eye -->
                                     <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
